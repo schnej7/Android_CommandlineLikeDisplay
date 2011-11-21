0b37d7981ab2c6d1da99482e52718d5b0da9dad5
schnej7 <schnej7@rpi.edu>
2011-11-21 09:38:21 -0500

    Adding files

---
+++ src/cld/CommandlineLikeDisplay/CommandlineLikeDisplayActivity.java
@@ -0,0 +1,78 @@
+package cld.CommandlineLikeDisplay;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+
+public class CommandlineLikeDisplayActivity extends Activity {
+	
+		private ArrayAdapter <String> mMessageArray;
+		private ListView mMessageView;
+		private ExampleService myService;
+		
+	    /** Called when the activity is first created. */
+	    @Override
+	    public void onCreate(Bundle savedInstanceState) {
+	        super.onCreate(savedInstanceState);
+	        setContentView(R.layout.main);
+	        setup();
+	    }
+	    
+	    //Used to setup the display and the service
+	    private void setup(){
+	    	mMessageArray = new ArrayAdapter<String>(this, R.layout.message);
+	    	mMessageView = (ListView) findViewById(R.id.ListMessages);
+	    	mMessageView.setAdapter(mMessageArray);
+	    	
+	    	myService = new ExampleService(mHandler, 10);
+	    	
+	        //button listener
+	        final Button buttonClear = (Button) findViewById(R.id.btnClear);
+	        buttonClear.setOnClickListener(new View.OnClickListener() {
+	            public void onClick(View v) {
+	                clearDisplay();
+	            }
+	        });
+	        
+	        //button listener
+	        final Button buttonStart = (Button) findViewById(R.id.btnStart);
+	        buttonStart.setOnClickListener(new View.OnClickListener() {
+	            public void onClick(View v) {
+	                myService.testFunction();
+	            }
+	        });
+	    }
+	    
+	    //Used to clear the display
+	    private void clearDisplay(){
+	    	mMessageArray.clear();
+	    }
+	    
+	    //Handler used for receiving messages from the service
+	    private final Handler mHandler = new Handler(){
+	    	@Override
+	    	public void handleMessage( Message msg ){
+	    		byte[] writeBuf;
+	    		String messageString;
+		    	switch(msg.what){
+		    	case Constants.MSG_DEBUG:
+		    		if(Constants.DEBUG){
+			    		writeBuf = (byte[]) msg.obj;
+			    		messageString = new String(writeBuf);
+			    		mMessageArray.add(messageString);
+		    		}
+		    		break;
+		    	case Constants.MSG_NORMAL:
+		    		writeBuf = (byte[]) msg.obj;
+		    		messageString = new String(writeBuf);
+		    		mMessageArray.add(messageString);
+		    		break;
+		    	}
+	    	}
+	    };
+	}
---
+++ src/cld/CommandlineLikeDisplay/ExampleService.java
@@ -0,0 +1,43 @@
+package cld.CommandlineLikeDisplay;
+
+import android.os.Handler;
+
+public class ExampleService {
+	
+	private int my_count_to;
+	private Handler myHandler;
+	
+	//example service constructor
+	public ExampleService( Handler a_Handler, int count_to ){
+		my_count_to = count_to;
+		myHandler = a_Handler;
+	}
+	
+	//start the example service
+	public void testFunction(){
+		for(int i = 0; i < my_count_to; i++){
+			
+			print_debug("Hello " + i);
+			
+		}
+	}
+	
+	//Print a debug message
+	public void print_debug(String outString) {
+		byte[] buffer = new byte[1024];
+		buffer = outString.getBytes();
+
+		myHandler.obtainMessage(Constants.MSG_DEBUG, buffer.length, -1, buffer)
+				.sendToTarget();
+	}
+
+	//Print a normal message
+	public void print_normal(String outString) {
+		byte[] buffer = new byte[1024];
+		buffer = outString.getBytes();
+
+		myHandler.obtainMessage(Constants.MSG_NORMAL, buffer.length, -1, buffer)
+				.sendToTarget();
+	}
+
+}
