e1a468e0295fa34b881d8a2c48cbf8a421b78158
schnej7 <schnej7@rpi.edu>
2011-11-23 02:27:23 -0500

    Implemented sample guessing game

--- src/cld/CommandlineLikeDisplay/ExampleService.java
+++ src/cld/CommandlineLikeDisplay/ExampleService.java
@@ -1,5 +1,7 @@
 package cld.CommandlineLikeDisplay;
 
+import java.util.Random;
+
 public class ExampleService {
 	
 	private CLDMessage myCLDMessage;
@@ -20,8 +22,16 @@ public class ExampleService {
 	//////////////////////////////////////////////////////////
 	
 	public void start(){
-		String input = myCLDMessage.getLine();
-		myCLDMessage.print_normal("You write: " + input);
+		myCLDMessage.print_normal("Guessing game 0 - 100");
+		int user_input = Integer.valueOf(myCLDMessage.getLine()).intValue();
+		Random a_random= new Random();
+		int rand = a_random.nextInt(100);
+		while (user_input != rand){
+			if(user_input > rand) myCLDMessage.print_normal("Too High!");
+			else myCLDMessage.print_normal("Too Low!");
+			user_input = Integer.valueOf(myCLDMessage.getLine()).intValue();
+		}
+		myCLDMessage.print_normal("You Win!");
 	}
 	//////////////////////////////////////////////////////////
 
